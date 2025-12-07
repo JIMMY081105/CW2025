ca3a4bddb0fddde676fbc1b3284bc455abc2edb1
JIMMY081105 <hfyym4@nottingham.edu.my>
2025-12-07 21:44:55 +0800

    79:removed the magic strings

--- src/main/java/com/comp2042/view/render/GameOverPanel.java
+++ src/main/java/com/comp2042/view/render/GameOverPanel.java
@@ -3,12 +3,14 @@ package com.comp2042.view.render;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
-public class GameOverPanel extends BorderPane {
+public final class GameOverPanel extends BorderPane {
+
+    private static final String GAME_OVER_TEXT = "GAME OVER";
+    private static final String GAME_OVER_STYLE_CLASS = "gameOverStyle";
 
     public GameOverPanel() {
-        final Label gameOverLabel = new Label("GAME OVER");
-        gameOverLabel.getStyleClass().add("gameOverStyle");
+        Label gameOverLabel = new Label(GAME_OVER_TEXT);
+        gameOverLabel.getStyleClass().add(GAME_OVER_STYLE_CLASS);
         setCenter(gameOverLabel);
     }
-
 }
