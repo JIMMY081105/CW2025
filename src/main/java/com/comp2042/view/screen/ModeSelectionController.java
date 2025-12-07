fa22dca8f9587703163c15b5307bf2943e3768bf
JIMMY081105 <hfyym4@nottingham.edu.my>
2025-12-07 22:08:02 +0800

    remove magic strings and numbers

--- src/main/java/com/comp2042/view/screen/ModeSelectionController.java
+++ src/main/java/com/comp2042/view/screen/ModeSelectionController.java
@@ -10,9 +10,26 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 
+import java.net.URL;
 import java.util.function.Consumer;
 
 public class ModeSelectionController {
+
+    // Font config
+    private static final String DIGITAL_FONT_RESOURCE = "digital.ttf";
+    private static final double DIGITAL_FONT_SIZE = 38.0;
+    
+    private static final String TITLE_ONE_MINUTE = "1 Minute Sprint";
+    private static final String TITLE_THREE_MINUTES = "3 Minute Rush";
+    private static final String TITLE_FIVE_MINUTES = "5 Minute Marathon";
+
+    private static final String BADGE_ONE_MINUTE = "1M";
+    private static final String BADGE_THREE_MINUTES = "3M";
+    private static final String BADGE_FIVE_MINUTES = "5M";
+
+    private static final String SUBTITLE_ONE_MINUTE = "Ultra fast, pure reaction";
+    private static final String SUBTITLE_THREE_MINUTES = "Balance speed and control";
+    private static final String SUBTITLE_FIVE_MINUTES = "Endurance and consistency";
 
     @FXML
     private StackPane selectionRoot;
@@ -71,13 +88,17 @@ public class ModeSelectionController {
     }
 
     private void loadFonts() {
-        Font.loadFont(
-                getClass().getClassLoader().getResource("digital.ttf").toExternalForm(),
-                38
-        );
+        URL fontUrl = getClass().getClassLoader().getResource(DIGITAL_FONT_RESOURCE);
+        if (fontUrl != null) {
+            Font.loadFont(fontUrl.toExternalForm(), DIGITAL_FONT_SIZE);
+        } else {
+            System.err.println("Missing font resource: " + DIGITAL_FONT_RESOURCE);
+        }
     }
 
-    public void configure(HomeSelection.Mode mode, Consumer<HomeSelection> selectionHandler, Runnable backHandler) {
+    public void configure(HomeSelection.Mode mode,
+                          Consumer<HomeSelection> selectionHandler,
+                          Runnable backHandler) {
         this.mode = mode;
         this.selectionHandler = selectionHandler;
         this.backHandler = backHandler;
@@ -87,32 +108,50 @@ public class ModeSelectionController {
     }
 
     private void setupTimeRacing() {
-        optionOneTitle.setText("1 Minute Sprint");
-        optionTwoTitle.setText("3 Minute Rush");
-        optionThreeTitle.setText("5 Minute Marathon");
+        if (optionOneTitle != null) {
+            optionOneTitle.setText(TITLE_ONE_MINUTE);
+        }
+        if (optionTwoTitle != null) {
+            optionTwoTitle.setText(TITLE_THREE_MINUTES);
+        }
+        if (optionThreeTitle != null) {
+            optionThreeTitle.setText(TITLE_FIVE_MINUTES);
+        }
 
-        badgeOne.setText("1M");
-        badgeTwo.setText("3M");
-        badgeThree.setText("5M");
+        if (badgeOne != null) {
+            badgeOne.setText(BADGE_ONE_MINUTE);
+        }
+        if (badgeTwo != null) {
+            badgeTwo.setText(BADGE_THREE_MINUTES);
+        }
+        if (badgeThree != null) {
+            badgeThree.setText(BADGE_FIVE_MINUTES);
+        }
 
-        optionOneSubtitle.setText("Ultra fast, pure reaction");
-        optionTwoSubtitle.setText("Balance speed and control");
-        optionThreeSubtitle.setText("Endurance and consistency");
+        if (optionOneSubtitle != null) {
+            optionOneSubtitle.setText(SUBTITLE_ONE_MINUTE);
+        }
+        if (optionTwoSubtitle != null) {
+            optionTwoSubtitle.setText(SUBTITLE_THREE_MINUTES);
+        }
+        if (optionThreeSubtitle != null) {
+            optionThreeSubtitle.setText(SUBTITLE_FIVE_MINUTES);
+        }
     }
 
     @FXML
     private void handleOptionOne() {
-        fireSelection(optionOneTitle.getText());
+        fireSelection(optionOneTitle != null ? optionOneTitle.getText() : null);
     }
 
     @FXML
     private void handleOptionTwo() {
-        fireSelection(optionTwoTitle.getText());
+        fireSelection(optionTwoTitle != null ? optionTwoTitle.getText() : null);
     }
 
     @FXML
     private void handleOptionThree() {
-        fireSelection(optionThreeTitle.getText());
+        fireSelection(optionThreeTitle != null ? optionThreeTitle.getText() : null);
     }
 
     @FXML
@@ -123,7 +162,7 @@ public class ModeSelectionController {
     }
 
     private void fireSelection(String option) {
-        if (selectionHandler != null && mode != null) {
+        if (selectionHandler != null && mode != null && option != null) {
             selectionHandler.accept(new HomeSelection(mode, option));
         }
     }
