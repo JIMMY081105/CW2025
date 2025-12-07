7f5307a0eecbdf971d346c74494a8e79ccf3a85d
JIMMY081105 <hfyym4@nottingham.edu.my>
2025-12-07 19:56:10 +0800

    removed unncecessary code and magic strings, and made pattern more effective

--- src/main/java/com/comp2042/util/BlockTextureProvider.java
+++ src/main/java/com/comp2042/util/BlockTextureProvider.java
@@ -8,22 +8,28 @@ import javafx.scene.paint.ImagePattern;
 import javafx.scene.paint.Paint;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class BlockTextureProvider {
 
     private static final int IMAGE_PADDING = 4;
-    private static final Map<Integer, Paint> PATTERNS = new HashMap<>();
+    private static final String BRICK_IMAGE_PATH_FORMAT = "images/Bricks/%s.png";
+
     private static final String[] IMAGE_NAMES = {
             null, "IBrick", "JBrick", "LBrick", "OBrick", "SBrick", "TBrick", "ZBrick", "PlusBrick"
     };
 
+    private static final Map<Integer, Paint> PATTERNS;
+
     static {
-        PATTERNS.put(0, Color.TRANSPARENT);
+        Map<Integer, Paint> patterns = new HashMap<>();
+        patterns.put(0, Color.TRANSPARENT);
         for (int id = 1; id < IMAGE_NAMES.length; id++) {
-            PATTERNS.put(id, loadPattern(id));
+            patterns.put(id, loadPattern(id));
         }
+        PATTERNS = Collections.unmodifiableMap(patterns);
     }
 
     private static Paint loadPattern(int id) {
@@ -31,18 +37,18 @@ public final class BlockTextureProvider {
         if (fileName == null) {
             return Color.TRANSPARENT;
         }
-        String path = String.format("images/Bricks/%s.png", fileName);
+
+        String path = String.format(BRICK_IMAGE_PATH_FORMAT, fileName);
         URL resource = BlockTextureProvider.class.getClassLoader().getResource(path);
         if (resource == null) {
             return Color.GRAY;
         }
-        Image image = new Image(resource.toExternalForm());
 
-        int targetSize = GameConstants.BRICK_SIZE;
+        Image image = new Image(resource.toExternalForm());
         int imgWidth = (int) image.getWidth();
         int imgHeight = (int) image.getHeight();
 
-        if (imgWidth > targetSize && imgHeight > targetSize) {
+        if (imgWidth > IMAGE_PADDING * 2 && imgHeight > IMAGE_PADDING * 2) {
             PixelReader reader = image.getPixelReader();
             int cropWidth = imgWidth - IMAGE_PADDING * 2;
             int cropHeight = imgHeight - IMAGE_PADDING * 2;
